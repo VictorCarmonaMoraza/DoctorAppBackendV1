c1b953b369402e1b984a07696a0a09435481833b
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-26 21:45:17 +0100

    6-Cadena de Conexion y agregar DbContext Class

---
+++ Data/DBContext/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace Data.DBContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Users { get; set; }         
+    }
+}
