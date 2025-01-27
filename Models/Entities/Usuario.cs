7a68f88b91e7ec0f31e285ee1f050ec87138c00a
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-27 21:33:43 +0100

    18-Actualizar entidad Usuario

--- Models/Entities/Usuario.cs
+++ Models/Entities/Usuario.cs
@@ -10,5 +10,8 @@ namespace Models.Entities
     {
         public int Id { get; set; }
         public string? Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
     }
 }
