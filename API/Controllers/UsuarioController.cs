78283f26c6cad1c6d9469e438d29243f5348a9bb
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-27 21:49:56 +0100

    20-Crear registro Endpoint

--- API/Controllers/UsuarioController.cs
+++ API/Controllers/UsuarioController.cs
@@ -1,7 +1,10 @@
 ﻿using Data.DBContext;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Models.DTOs;
 using Models.Entities;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace API.Controllers
 {
@@ -22,7 +25,7 @@ namespace API.Controllers
         [HttpGet]  //api/usuario
         public async Task<ActionResult<IEnumerable<Usuario>>> GetUsuarios()
         {
-            var usuarios =await  _context.Users.ToListAsync();
+            var usuarios = await _context.Users.ToListAsync();
             return Ok(usuarios);
         }
 
@@ -32,7 +35,7 @@ namespace API.Controllers
         /// <param name="id"></param>
         /// <returns></returns>
         [HttpGet("{id}")] //api/usuario
-        public async  Task<ActionResult<Usuario>> GetUsuario(int id)
+        public async Task<ActionResult<Usuario>> GetUsuario(int id)
         {
             var usuario = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
             if (usuario == null)
@@ -41,6 +44,38 @@ namespace API.Controllers
             }
 
             return Ok(usuario);
+        }
+
+        /// <summary>
+        /// Registra un usuario en la base de datos
+        /// </summary>
+        /// <param name="registroDto"></param>
+        /// <returns></returns>
+        [HttpPost("registro")]
+        public async Task<ActionResult<Usuario>> Registro(RegistroDto registroDto)
+        {
+            if (await UsuarioExiste(registroDto.Username)) return BadRequest("El usuario ya esta registrado en la base de datos");
+
+            using var hmac = new HMACSHA512();
+            var usuario = new Usuario
+            {
+                Username = registroDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registroDto.Pasword)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(usuario);
+            await _context.SaveChangesAsync();
+            return usuario;
+        }
+
+        /// <summary>
+        /// Valida si un usuario existe en la base de datos
+        /// </summary>
+        /// <param name="username"></param>
+        /// <returns></returns>
+        private async Task<bool> UsuarioExiste(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.Username == username.ToLower());
         }
     }
 }
