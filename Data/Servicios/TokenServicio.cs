2f5e5c694684d7175720579c948a9e4574c3aa40
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-28 19:10:27 +0100

    24-Agregar logivca de crear token

--- Data/Servicios/TokenServicio.cs
+++ Data/Servicios/TokenServicio.cs
@@ -1,18 +1,43 @@
-﻿using Data.Interfaces;
+﻿
+
+using Data.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Models.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Data.Servicios
 {
     public class TokenServicio : ITokenServicio
     {
+
+        private readonly Microsoft.IdentityModel.Tokens.SymmetricSecurityKey _key;
+
+        public TokenServicio(IConfiguration config)
+        {
+            _key=new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
         public string CreatrToken(Usuario usuario)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.Username)
+            };
+
+            var credenciales = new SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credenciales
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
