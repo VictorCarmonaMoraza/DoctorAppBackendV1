e24f18cc9ae8605ef254ec6cc7f78463ec3a8996
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-28 19:57:23 +0100

    27-Agregar Metodos de Extension

---
+++ API/Extensiones/ServicioAplicacionExtension.cs
@@ -0,0 +1,54 @@
+﻿using Data.DBContext;
+using Data.Interfaces;
+using Data.Servicios;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace API.Extensiones
+{
+    public static class ServicioAplicacionExtension
+    {
+        public static IServiceCollection AgregarServiciosAplicacion(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(option =>
+             {
+
+                 option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                 {
+                     Description = "Ingresar Bearer [espacio] token \r\n\r|\n "+
+                     "Ejemplo: Bearer ejoydfoifiosdfiousdoifu",
+                     Name = "Authorization",
+                     In = ParameterLocation.Header,
+                     //Type = SecuritySchemeType.ApiKey,
+                     Scheme = "Bearer"
+                 });
+                 option.AddSecurityRequirement(new OpenApiSecurityRequirement
+     {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header
+            },
+            new List<string>()
+        }
+     });
+             });
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            services.AddCors();
+
+            services.AddScoped<ITokenServicio, TokenServicio>();
+
+            return services;
+        }
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,6 @@
-using Data.DBContext;
-using Data.Interfaces;
-using Data.Servicios;
+using API.Extensiones;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,56 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(option =>
-{
-
-    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Description = "Ingresar Bearer [espacio] token \r\n\r|\n "+
-        "Ejemplo: Bearer ejoydfoifiosdfiousdoifu",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        //Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer"
-    });
-    option.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                },
-                Scheme = "oauth2",
-                Name = "Bearer",
-                In = ParameterLocation.Header
-            },
-            new List<string>()
-        }
-    });
-});
-
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
-builder.Services.AddCors();
-
-builder.Services.AddScoped<ITokenServicio, TokenServicio>();
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
+builder.Services.AgregarServiciosAplicacion(builder.Configuration);
+builder.Services.AgregarServiciosIdentidad(builder.Configuration);
 
 var app = builder.Build();
 
