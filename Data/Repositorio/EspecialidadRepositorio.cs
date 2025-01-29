daaf0c5835c21415a056e6fb634ce68376cc53de
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-29 21:11:02 +0100

    47-Especialidad Repositorio

---
+++ Data/Repositorio/EspecialidadRepositorio.cs
@@ -0,0 +1,33 @@
+﻿using Data.DBContext;
+using Data.Interfaces.IRepositorio;
+using Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repositorio
+{
+    public class EspecialidadRepositorio : Repositorio<Especialidad>, IEspecialidadRepositorio
+    {
+        private readonly ApplicationDbContext _contexto;    
+        public EspecialidadRepositorio(ApplicationDbContext contexto) : base(contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Actualizar(Especialidad especialidad)
+        {
+            var especialidadDb = _contexto.Especialidades.FirstOrDefault(x => x.Id == especialidad.Id);
+            if(especialidadDb != null)
+            {
+                especialidadDb.NombreEspecialidad = especialidad.NombreEspecialidad;
+                especialidadDb.Descripcion = especialidad.Descripcion;
+                especialidadDb.Estado = especialidad.Estado;
+                _contexto.SaveChanges();
+            }
+        }
+
+    }
+}
