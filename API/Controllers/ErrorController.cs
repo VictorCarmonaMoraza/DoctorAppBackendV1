b72f712fbf0c694a960ec21a3fc21ddc2a1b6e5b
Victor <az_v.carmona.ext@W4ALL.SERVICES>
2025-01-29 18:43:47 +0100

    40-Agregar un manejador de errores para un Not Found endpoint

---
+++ API/Controllers/ErrorController.cs
@@ -0,0 +1,18 @@
+﻿using API.Errores;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace API.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("errores/{codigo}")]
+    //Ignora en la documentacion de swagger
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController: BaseApiController
+    {
+        public IActionResult Error(int codigo)
+        {
+            return new ObjectResult(new ApiErrorResponse(codigo));
+        }
+    }
+}
